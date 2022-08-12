717620d01606cc4a7b190fb7b5e87963d86415e2
raioramalho <ramalho.sit@gmail.com>
2022-08-12 18:28:15 -0300

    Pág show added - pag of loged user

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :contacts
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   
 
 end
