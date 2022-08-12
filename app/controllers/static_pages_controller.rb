e894970ccd58f8623b2ac131ba5f40ae9b42f093
raioramalho <ramalho.sit@gmail.com>
2022-08-11 21:14:14 -0300

    bootstrap links: OK!

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,10 @@ class StaticPagesController < ApplicationController
     def index
         @nome = "Alan Ramalho"
     end
+
+    def sobre
+    end
+
+    def contato
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "static_pages#index"
+
+  get 'sobre', to: "static_pages#sobre"
+  get 'contato', to: "static_pages#contato"
   # Defines the root path route ("/")
   # root "articles#index"
 end
