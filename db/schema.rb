0bae56c05b9974ba2cf6f57a9981d4b960f1ef34
raioramalho <ramalho.sit@gmail.com>
2022-08-12 19:46:40 -0300

    added changes to contacts tab, added ref to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_12_005850) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_12_224504) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_12_005850) do
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_contacts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,4 +31,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_12_005850) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "contacts", "users"
 end
