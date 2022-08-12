692531534d403242b298f3c5dccfea3235dac78b
raioramalho <ramalho.sit@gmail.com>
2022-08-12 18:20:31 -0300

    Sessions_controller updated and Ready!

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,11 @@
 class SessionsController < ApplicationController
   def new
-    
   end
 
   def create
-
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password]) #has_secure_password
+      sing_in(user)
+    end
   end
 end
