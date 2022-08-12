afbd52c1d8d812ef25feb6348353cba0c7819e54
raioramalho <ramalho.sit@gmail.com>
2022-08-11 22:01:49 -0300

    Tabela users, renomeada a coluna nome para name

--- db/migrate/20220812004229_create_users.rb
+++ db/migrate/20220812004229_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :email, null: false, default: ''
-      t.string :nome, null: false, default: ''
+      t.string :name, null: false, default: ''
       t.string :password_digest, null: false, default: ''
 
       t.timestamps
