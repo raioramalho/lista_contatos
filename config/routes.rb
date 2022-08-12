e614dd3de969fd95a761634764cd6337147e409e
raioramalho <ramalho.sit@gmail.com>
2022-08-12 18:09:49 -0300

    added sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "static_pages#index"
 
   get 'sobre', to: "static_pages#sobre"
   get 'contato', to: "static_pages#contato"
+  get 'entrar', to: 'sessions#new'
+  post 'entrar', to: 'sessions#create'
   # Defines the root path route ("/")
   # root "articles#index"
   resources :contacts
   resources :users, only: [:new, :create]
+  
 
 end
