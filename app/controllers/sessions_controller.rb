f2b067d64cd1aec5be871f3349850898f5e83669
raioramalho <ramalho.sit@gmail.com>
2022-08-12 18:23:22 -0300

    Mensagem de erro e redenrização se não estiver ok

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,11 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password]) #has_secure_password
       sing_in(user)
+      redirect_to user_path(user)
+    else
+      flash.now[:danger]  = 'Email ou senha invalidos'
+      render 'new'
     end
   end
+
 end
